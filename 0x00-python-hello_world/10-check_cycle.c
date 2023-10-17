c4347bf5575080db30f0e177a2564e923a18802b
EfiokobongN <mailpreciousnelson@gmail.com>
2023-10-17 03:58:31 -0700

    alx project

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+/**
+ * check_cycle - checks if singly linked list has a cycle.
+ * @list: pointer to beginning of node
+ * Return: 0 if no cycle, else 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *check;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
+
+	while (current != NULL && check->next != NULL
+		&& check->next->next != NULL)
+	{
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
+	}
+	return (0);
+}
